9d37631d8e6ae87ea08215395dc601fc47743b3a
averyewilson <155782172+averyewilson@users.noreply.github.com>
2024-01-17 14:54:43 -0800

    Updated Form Conformation Page

--- app.py
+++ app.py
@@ -2,31 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-
-
-
-# the homepage for your site will be at the route below
-@app.route('/')
-def hello_world():
-    return """
-    <h1>Hello World!</h1> 
-    <br />
-    Try these links: <br /> 
-     - <a href='/home'>home</a><br />
-     - <a href='/bootstrap'>bootstrap</a><br />
-    """
-
-# Rendering a html template
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-# Rendering a html template with the Bootstrap framework
-@app.route('/bootstrap')
-def bootstrap():
-    return render_template("bootstrap.html")
+@app.route('/recipes')
+def recipes():
+    return render_template('recipes.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
